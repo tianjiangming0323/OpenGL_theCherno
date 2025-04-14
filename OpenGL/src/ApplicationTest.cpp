6b7c832a19ade7c09d5f394c8eb58dc1223deba6
WangHD <1010639858@qq.com>
2025-04-14 16:23:06 +0800

    028 An Introduction to Batch Rendering & I Rename My Applications.cpps with Different Functions

--- OpenGL/src/ApplicationTest.cpp
+++ OpenGL/src/ApplicationTest.cpp
@@ -1,133 +1,133 @@
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-
-#include "Renderer.h"
-#include "IndexBuffer.h"
-#include "VertexBuffer.h"
-#include "VertexArray.h"
-#include "VertexBufferLayout.h"
-#include "Shader.h"
-#include "Texture.h"
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw_gl3.h"
-
-#include "tests/TestClearColor.h"
-#include "tests/TestTexture2D.h"
-#include "tests/TestRectangle.h"
-
-
-int main(void)
-{
-    GLFWwindow* window;
-    
-    if (!glfwInit())
-        return -1;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);   
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
-
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-   
-    glfwMakeContextCurrent(window);
-    
-    glfwSwapInterval(1);
-    
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error !" << std::endl;
-
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
-    {
-        
-        GLCALL(glEnable(GL_BLEND));
-        GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        Renderer renderer;
-
-        ImGui::CreateContext();
-        ImGui_ImplGlfwGL3_Init(window, true);
-        ImGui::StyleColorsDark();
-   
-        //S25Ìí¼Ó
-        test::Test* currentTest = nullptr;
-        test::TestMenu* testMenu = new test::TestMenu(currentTest);
-        currentTest = testMenu;
-        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
-        testMenu->RegisterTest<test::TestTexture2D>("Texture2D");
-        testMenu->RegisterTest<test::TestRectangle>("Test Rectangle");
-        
-
-        //S24Ìí¼Ó
-        //S25×¢ÊÍ
-        //test::TestClearColor test;
- 
-        while (!glfwWindowShouldClose(window))
-        {
-
-            renderer.Clear();
-
-            //S24Ìí¼Ó
-            //S25×¢ÊÍ
-            //test.OnUpdate(0.0f);
-            //test.OnRender();
-
-
-            ImGui_ImplGlfwGL3_NewFrame();
-
-            //S25Ìí¼Ó
-            if (currentTest)
-            {  
-                currentTest->OnUpdate(0.0f);
-                currentTest->OnRender();
-                ImGui::Begin("Test");
-                if (currentTest != testMenu && ImGui::Button("<-"))
-                {
-                    delete currentTest;
-                    currentTest = testMenu;
-                }
-                currentTest->OnImGuiRender();
-                ImGui::End();
-            }
-           
-
-            //S24Ìí¼Ó
-            //S25×¢ÊÍ
-            //test.OnImGuiRender();
-
-            ImGui::Render();
-            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-           
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
-
-        //S25Ìí¼Ó
-        delete currentTest;
-        if (currentTest != testMenu)
-            delete testMenu;
-       
-    }
-
-    ImGui_ImplGlfwGL3_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwTerminate();
-    return 0;
-}
+//#include "GL/glew.h"
+//#include "GLFW/glfw3.h"
+//
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//#include <sstream>
+//
+//#include "Renderer.h"
+//#include "IndexBuffer.h"
+//#include "VertexBuffer.h"
+//#include "VertexArray.h"
+//#include "VertexBufferLayout.h"
+//#include "Shader.h"
+//#include "Texture.h"
+//
+//#include "glm/glm.hpp"
+//#include "glm/gtc/matrix_transform.hpp"
+//
+//#include "imgui/imgui.h"
+//#include "imgui/imgui_impl_glfw_gl3.h"
+//
+//#include "tests/TestClearColor.h"
+//#include "tests/TestTexture2D.h"
+//#include "tests/TestRectangle.h"
+//
+//
+//int main(void)
+//{
+//    GLFWwindow* window;
+//    
+//    if (!glfwInit())
+//        return -1;
+//
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);   
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
+//
+//    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//   
+//    glfwMakeContextCurrent(window);
+//    
+//    glfwSwapInterval(1);
+//    
+//    if (glewInit() != GLEW_OK)
+//        std::cout << "Error !" << std::endl;
+//
+//    std::cout << glGetString(GL_VERSION) << std::endl;
+//
+//    {
+//        
+//        GLCALL(glEnable(GL_BLEND));
+//        GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+//
+//        Renderer renderer;
+//
+//        ImGui::CreateContext();
+//        ImGui_ImplGlfwGL3_Init(window, true);
+//        ImGui::StyleColorsDark();
+//   
+//        //S25Ìí¼Ó
+//        test::Test* currentTest = nullptr;
+//        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+//        currentTest = testMenu;
+//        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+//        testMenu->RegisterTest<test::TestTexture2D>("Texture2D");
+//        testMenu->RegisterTest<test::TestRectangle>("Test Rectangle");
+//        
+//
+//        //S24Ìí¼Ó
+//        //S25×¢ÊÍ
+//        //test::TestClearColor test;
+// 
+//        while (!glfwWindowShouldClose(window))
+//        {
+//
+//            renderer.Clear();
+//
+//            //S24Ìí¼Ó
+//            //S25×¢ÊÍ
+//            //test.OnUpdate(0.0f);
+//            //test.OnRender();
+//
+//
+//            ImGui_ImplGlfwGL3_NewFrame();
+//
+//            //S25Ìí¼Ó
+//            if (currentTest)
+//            {  
+//                currentTest->OnUpdate(0.0f);
+//                currentTest->OnRender();
+//                ImGui::Begin("Test");
+//                if (currentTest != testMenu && ImGui::Button("<-"))
+//                {
+//                    delete currentTest;
+//                    currentTest = testMenu;
+//                }
+//                currentTest->OnImGuiRender();
+//                ImGui::End();
+//            }
+//           
+//
+//            //S24Ìí¼Ó
+//            //S25×¢ÊÍ
+//            //test.OnImGuiRender();
+//
+//            ImGui::Render();
+//            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+//
+//           
+//            glfwSwapBuffers(window);
+//            glfwPollEvents();
+//        }
+//
+//        //S25Ìí¼Ó
+//        delete currentTest;
+//        if (currentTest != testMenu)
+//            delete testMenu;
+//       
+//    }
+//
+//    ImGui_ImplGlfwGL3_Shutdown();
+//    ImGui::DestroyContext();
+//
+//    glfwTerminate();
+//    return 0;
+//}
